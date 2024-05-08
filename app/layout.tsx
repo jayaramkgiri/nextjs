ed05f90cfad6b326bf0859665f7c18486035fcf3
jayaramkgiri <jayaram.cit@gmail.com>
2024-05-08 23:59:58 +0530

    sidebar rollback

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,13 +12,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden ">
-          <div className="w-full flex-none md:w-64">
+        <div className="flex h-screen w-full flex-col md:flex-row md:overflow-hidden ">
+          <div className="w-full flex-none md:w-1/6">
             <SideNav />
           </div>
-          <div className="h-[1024px] w-0.5 relative bg-whitesmoke-100 mq975:hidden" />
-      <div className="h-[1024px] w-0.5 relative bg-whitesmoke-200 mq975:hidden" />
-          <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+          <div className="hidden md:block h-screen w-0.5 relative bg-whitesmoke-100" />
+          <div className="hidden md:block h-screen w-0.5 relative bg-whitesmoke-200" />
+          <div className="h-auto md:hidden h-auto w-full overflow-hidden shrink-0 flex flex-row items-center justify-start">
+            <Cards rupee="/rupee-40.svg" totalBid="Total Bids" />
+            <Cards rupee="/rupee-41.svg" totalBid="Total Asks" propColor="#eb5757" />
+          </div>
+          <div className="grow p-6 md:overflow-y-auto md:px-10">{children}</div>
+          <div className="h-screen w-0.5 relative bg-whitesmoke-100" />
+          <div className="h-screen w-0.5 relative bg-whitesmoke-200" />
+          <div className="h-auto hidden md:block h-screen md:w-1/5 overflow-hidden shrink-0 flex flex-col items-start justify-start mx-auto box-border space-y-4">
+            <Cards totalBid="Total Bids" />
+            <Cards totalBid="Total Asks" propColor="#eb5757" />
+          </div>
         </div>
       </body>
     </html>
