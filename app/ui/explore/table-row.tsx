76074c1be11662fc6103e9bf1594c3dfaccc161b
jayaramkgiri <jayaram.cit@gmail.com>
2024-05-20 02:16:17 +0530

    cell keys

--- app/ui/explore/table-row.tsx
+++ app/ui/explore/table-row.tsx
@@ -28,9 +28,12 @@ export default function TableRow({
       className="text-sm w-full border-b py-3 last-of-type:border-none hover:cursor-pointer hover:bg-slate-50 [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
       onClick={() => router.push('/overview')}
     >
-      {cells?.map((cell) => {
+      {cells?.map((cell, index) => {
         return (
-          <td className="shadow-r-inner sticky left-0 mx-0 border-spacing-x-1 whitespace-nowrap border border-l-0 border-solid border-gray-200 bg-auto px-3 py-3">
+          <td
+            key={index}
+            className="shadow-r-inner sticky left-0 mx-0 border-spacing-x-1 whitespace-nowrap border border-l-0 border-solid border-gray-200 bg-auto px-3 py-3"
+          >
             {row[cell]}
           </td>
         );
