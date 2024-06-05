609363f356b9bb30177abb26cf4fe948d1942ede
jayaramkgiri <jayaram.cit@gmail.com>
2024-06-06 02:17:52 +0530

    tab fix

--- app/ui/explore/top-nav.tsx
+++ app/ui/explore/top-nav.tsx
@@ -18,15 +18,15 @@ export default function TopNav() {
             key={link.name}
             className={`text-13xl group w-full  ${
               pathname !== link.href &&
-              'hover:duration-250 transition duration-0 hover:border-b-2'
+              'hover:duration-250  transition duration-0 hover:border-b-2 hover:border-solid hover:border-indianred'
             }`}
           >
             <Link
               key={link.name}
               href={link.href}
-              className={`mx-auto block  pb-3 font-medium no-underline  ${
+              className={`mx-auto block pb-3 font-medium no-underline  ${
                 pathname === link.href
-                  ? 'border-b-2 border-chocolate text-gray-800'
+                  ? 'border-b-2 border-solid border-indianred text-gray-800'
                   : ' text-gray-300 group-hover:text-gray-800'
               }`}
             >
