07a4f439d5feeb76385b4c01e76ace81f6f0660d
jayaramkgiri <jayaram.cit@gmail.com>
2024-07-17 13:48:46 +0530

    debenture details init

---
+++ app/explore/debentures/[id]/page.tsx
@@ -0,0 +1,238 @@
+//       <main className="flex-1 bg-neutral-white overflow-hidden flex flex-row items-start justify-start pt-8 px-8 pb-0 box-border gap-[32px] max-w-[calc(100%_-_280px)] shrink-0 mq700:gap-[16px] mq975:flex-wrap mq975:max-w-full">
+
+//         <div className="w-[392px] shadow-[0px_1px_10px_rgba(0,_0,_0,_0.12),_0px_4px_5px_rgba(0,_0,_0,_0.14),_0px_2px_4px_-1px_rgba(0,_0,_0,_0.2)] overflow-hidden shrink-0 flex flex-col items-start justify-start gap-[32px] min-w-[392px] max-w-full mq700:min-w-full mq975:flex-1 mq450:gap-[16px]">
+//           <div className="self-stretch h-[521px] shadow-[0px_1px_10px_rgba(0,_0,_0,_0.12),_0px_4px_5px_rgba(0,_0,_0,_0.14),_0px_2px_4px_-1px_rgba(0,_0,_0,_0.2)] rounded-xl bg-neutral-white flex flex-col items-start justify-start pt-0 px-0 pb-0 box-border gap-[10px] max-w-full mq450:h-auto">
+//             <div className="self-stretch shadow-[0px_1px_10px_rgba(0,_0,_0,_0.12),_0px_4px_5px_rgba(0,_0,_0,_0.14),_0px_2px_4px_-1px_rgba(0,_0,_0,_0.2)] bg-neutral-50 flex flex-col items-start justify-start pt-4 px-4 pb-[9px] shrink-0">
+//               <h3 className="m-0 self-stretch relative text-xl leading-[155%] font-semibold font-h3 text-text-tertiary text-left mq450:text-base mq450:leading-[25px]">
+//                 CASHFLOW
+//               </h3>
+//               <div className="self-stretch relative text-xs leading-[16px] font-medium font-h3 text-text-tertiary whitespace-pre-wrap text-left">
+//                 Next Payment cutoff : 24th June
+//               </div>
+//             </div>
+//             <div className="self-stretch flex flex-row items-start justify-start p-4 box-border min-h-[878px] max-w-full shrink-0">
+//               <div className="flex-1 flex flex-col items-start justify-start gap-[8px] max-w-full">
+//                 <nav className="m-0 self-stretch flex flex-row items-start justify-start [row-gap:20px] whitespace-nowrap mq450:flex-wrap">
+//                   <div className="flex-1 relative text-base leading-[150%] font-semibold font-h3 text-darkgray text-left inline-block min-w-[52px] max-w-[90px]">
+//                     Date
+//                   </div>
+//                   <div className="flex-1 relative text-base leading-[150%] font-semibold font-h3 text-darkgray text-left inline-block min-w-[52px] max-w-[90px]">
+//                     Principal
+//                   </div>
+//                   <div className="flex-1 relative text-base leading-[150%] font-semibold font-h3 text-darkgray text-left inline-block min-w-[52px] max-w-[90px]">
+//                     Interest
+//                   </div>
+//                   <div className="flex-1 relative text-base leading-[150%] font-semibold font-h3 text-darkgray text-left inline-block min-w-[52px] max-w-[90px]">
+//                     Total
+//                   </div>
+//                 </nav>
+//                 <div className="self-stretch flex flex-col items-start justify-start pt-0 px-0 pb-0 box-border min-h-[48px]">
+//                   <div className="self-stretch flex flex-row items-center justify-start [row-gap:20px] mq450:flex-wrap">
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       10,000
+//                     </div>
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       10,000,000
+//                     </div>
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       <p className="m-0">9.2%</p>
+//                     </div>
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       <p className="m-0">2.34 Cr</p>
+//                     </div>
+//                   </div>
+//                   <img
+//                     className="self-stretch relative max-w-full overflow-hidden max-h-full mt-[-3px]"
+//                     loading="lazy"
+//                     alt=""
+//                     src="/vector-37-2.svg"
+//                   />
+//                 </div>
+//                 <div className="self-stretch flex flex-col items-start justify-start pt-0 px-0 pb-0 box-border min-h-[48px]">
+//                   <div className="self-stretch flex flex-row items-center justify-start [row-gap:20px] mq450:flex-wrap">
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       10,000
+//                     </div>
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       10,000,000
+//                     </div>
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       <p className="m-0">9.2%</p>
+//                     </div>
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       <p className="m-0">2.34 Cr</p>
+//                     </div>
+//                   </div>
+//                   <img
+//                     className="self-stretch relative max-w-full overflow-hidden max-h-full mt-[-3px]"
+//                     loading="lazy"
+//                     alt=""
+//                     src="/vector-37-2.svg"
+//                   />
+//                 </div>
+//                 <div className="self-stretch flex flex-col items-start justify-start pt-0 px-0 pb-0 box-border min-h-[48px]">
+//                   <div className="self-stretch flex flex-row items-center justify-start [row-gap:20px] mq450:flex-wrap">
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       10,000
+//                     </div>
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       10,000,000
+//                     </div>
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       <p className="m-0">9.2%</p>
+//                     </div>
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       <p className="m-0">2.34 Cr</p>
+//                     </div>
+//                   </div>
+//                   <img
+//                     className="self-stretch relative max-w-full overflow-hidden max-h-full mt-[-3px]"
+//                     loading="lazy"
+//                     alt=""
+//                     src="/vector-37-2.svg"
+//                   />
+//                 </div>
+//                 <div className="self-stretch flex flex-col items-start justify-start pt-0 px-0 pb-0 box-border min-h-[48px]">
+//                   <div className="self-stretch flex flex-row items-center justify-start [row-gap:20px] mq450:flex-wrap">
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       10,000
+//                     </div>
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       10,000,000
+//                     </div>
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       <p className="m-0">9.2%</p>
+//                     </div>
+//                     <div className="h-12 flex-1 relative text-sm leading-[142%] font-h3 text-dimgray-200 text-left inline-block min-w-[58px] max-w-[90px]">
+//                       <p className="m-0">2.34 Cr</p>
+//                     </div>
+//                   </div>
+//                   <img
+//                     className="self-stretch relative max-w-full overflow-hidden max-h-full mt-[-3px]"
+//                     alt=""
+//                     src="/vector-37-2.svg"
+//                   />
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="self-stretch shadow-[0px_1px_10px_rgba(0,_0,_0,_0.12),_0px_4px_5px_rgba(0,_0,_0,_0.14),_0px_2px_4px_-1px_rgba(0,_0,_0,_0.2)] rounded-xl bg-neutral-white overflow-hidden flex flex-col items-start justify-start">
+//             <div className="self-stretch bg-neutral-white flex flex-col items-start justify-start gap-[2px]">
+//               <div className="self-stretch bg-neutral-50 flex flex-col items-start justify-start pt-4 px-4 pb-[17px]">
+//                 <div className="self-stretch relative text-xl leading-[155%] font-semibold font-h3 text-text-tertiary text-left mq450:text-base mq450:leading-[25px]">
+//                   Files
+//                 </div>
+//               </div>
+//               <div className="self-stretch bg-neutral-1001 flex flex-row items-start justify-start gap-[2px] mq450:flex-wrap">
+//                 <FaceValue
+//                   informationMemorandum="Information Memorandum"
+//                   image1="/image-1@2x.png"
+//                 />
+//                 <FaceValue
+//                   informationMemorandum="Trust Deed"
+//                   image1="/image-1-1@2x.png"
+//                   propMinWidth="unset"
+//                 />
+//                 <FaceValue
+//                   informationMemorandum="Latest Rating"
+//                   image1="/image-1-2@2x.png"
+//                   propMinWidth="97.3px"
+//                 />
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </main>
+//     </form>
+//   );
+// };
+
+export default async function Page() {
+  return (
+    <section className="mq700:pb-[82px] mq700:box-border mq700:min-w-full mq975:gap-[16px] mq975:pb-[126px] mq975:box-border box-border flex max-w-full flex-1 flex-col items-start justify-start gap-[32px] px-0 pb-[194px] pt-0">
+      <div className="mq700:flex-wrap flex max-w-full flex-row items-start justify-start gap-[10px] self-stretch overflow-hidden">
+        <div className="flex w-[219px] flex-col items-start justify-start gap-[10px]">
+          <b className="font-h3 text-brand-primary mq975:text-5xl mq975:leading-[32px] mq450:text-lg mq450:leading-[24px] relative text-left text-[30px] leading-[40px]">
+            INE196P07047
+          </b>
+          <div className="font-h3 text-brand-primary relative self-stretch text-left text-xs font-light leading-[16px]">
+            BSE: 938742
+          </div>
+          <div className="font-h3 text-brand-primary relative self-stretch text-left text-xs font-light leading-[16px]">
+            NSE: 871REC28-N9
+          </div>
+        </div>
+        <div className="mq700:min-h-[auto] box-border flex min-h-[92px] min-w-[335px] max-w-full flex-1 flex-col items-start justify-between overflow-hidden py-0 pl-8 pr-5">
+          <div className="font-h3 relative inline-block max-w-full text-left text-lg font-semibold leading-[30px] text-blue-500">{`PIRAMAL CAPITAL & HOUSING FINANCE LIMITED`}</div>
+          <div className="font-h3 text-text-tertiary1 relative inline-block min-w-[101px] text-left text-lg font-semibold leading-[30px]">
+            CRISIL AAA
+          </div>
+        </div>
+      </div>
+      <div className="bg-neutral-white flex flex-col items-start justify-start self-stretch overflow-hidden rounded-xl shadow-[0px_1px_10px_rgba(0,_0,_0,_0.12),_0px_4px_5px_rgba(0,_0,_0,_0.14),_0px_2px_4px_-1px_rgba(0,_0,_0,_0.2)]">
+        <div className="bg-neutral-white flex flex-col items-start justify-start gap-[2px] self-stretch">
+          <div className="flex flex-col items-start justify-start self-stretch bg-neutral-50 px-4 pb-[17px] pt-4">
+            <div className="font-h3 text-text-tertiary mq450:text-base mq450:leading-[25px] relative self-stretch text-left text-xl font-semibold leading-[155%]">
+              ISSUE DETAILS
+            </div>
+          </div>
+          <div className="bg-neutral-1001 mq700:flex-wrap flex flex-row items-start justify-center gap-[2px] self-stretch">
+            <p>97100</p>
+            <div className="bg-neutral-white box-border flex h-[102px] min-w-[144px] max-w-[147px] flex-1 flex-col items-start justify-between overflow-hidden p-4">
+              <div className="text-sm font-h3 text-text-icon relative flex h-[35px] items-center self-stretch text-left font-semibold leading-[142%]">
+                Allotment Date
+              </div>
+              <div className="flex flex-row items-start justify-start px-0 pb-[3px] pt-0">
+                <div className="font-typography-h6 mq450:text-base mq450:leading-[26px] relative inline-block min-w-[118px] whitespace-nowrap text-left text-xl font-medium leading-[160%] tracking-[0.15px] text-text-secondary">
+                  27 Jan, 2020
+                </div>
+              </div>
+            </div>
+            <p>97100</p>
+            <div className="bg-neutral-white box-border flex h-[102px] min-w-[144px] max-w-[147px] flex-1 flex-col items-start justify-between overflow-hidden py-4 pl-4 pr-[15px]">
+              <div className="text-sm font-h3 text-text-icon relative flex h-[35px] items-center self-stretch text-left font-semibold leading-[142%]">
+                Maturity / Redemption
+              </div>
+              <div className="z-[1] flex flex-row items-start justify-start px-0 pb-[3px] pt-0">
+                <div className="font-typography-h6 mq450:text-base mq450:leading-[26px] relative inline-block min-w-[118px] whitespace-nowrap text-left text-xl font-medium leading-[160%] tracking-[0.15px] text-text-secondary">
+                  27 Jan, 2025
+                </div>
+              </div>
+            </div>
+            <div className="bg-neutral-white box-border flex min-w-[144px] max-w-[147px] flex-1 flex-col items-start justify-start overflow-hidden p-4">
+              <div className="text-sm font-h3 text-text-icon relative flex h-[35px] items-center self-stretch text-left font-semibold leading-[142%]">
+                Coupon
+              </div>
+              <div className="flex flex-row items-start justify-start self-stretch px-0 pb-[3px] pt-0">
+                <div className="font-typography-h6 mq450:text-base mq450:leading-[26px] relative inline-block min-w-[56px] text-left text-xl font-medium leading-[160%] tracking-[0.15px] text-text-secondary">{`10.5% `}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-neutral-white flex max-w-full flex-col items-start justify-start self-stretch overflow-hidden rounded-xl shadow-[0px_1px_10px_rgba(0,_0,_0,_0.12),_0px_4px_5px_rgba(0,_0,_0,_0.14),_0px_2px_4px_-1px_rgba(0,_0,_0,_0.2)]">
+        <div className="box-border flex max-w-full flex-col items-start justify-start self-stretch bg-neutral-50 p-4">
+          <h3 className="font-h3 text-text-tertiary mq450:text-base mq450:leading-[16px] relative m-0 self-stretch text-left text-xl font-semibold leading-[20px]">
+            MARKET DEPTH
+          </h3>
+          <div className="mq700:flex-wrap mq700:gap-[16px] flex max-w-full flex-row items-start justify-start gap-[32px] self-stretch">
+            <div className="font-h3 text-text-tertiary relative inline-block min-w-[99px] whitespace-nowrap text-left text-xs font-medium leading-[16px]">{`Interest Accrued:  `}</div>
+            <div className="mq700:pr-[113px] mq700:box-border mq700:min-w-full mq450:pr-5 mq450:box-border box-border flex h-5 min-w-[350px] max-w-full flex-1 flex-row items-start justify-start gap-[4px] py-0 pl-0 pr-[227px]">
+              <div className="flex flex-col items-center justify-start px-0 py-1.5">
+                <img
+                  className="relative h-[9px] w-1.5"
+                  loading="lazy"
+                  alt=""
+                  src="/rupee-21.svg"
+                />
+              </div>
+              <div className="font-h3 text-text-tertiary relative flex h-5 max-w-[calc(100%_-_10px)] flex-1 items-center text-left text-xs font-medium leading-[133%]">
+                342
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
