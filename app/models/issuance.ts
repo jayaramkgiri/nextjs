d80a2cea772d8ee542aa04107b629321288ef96c
jayaramkgiri <jayaram.cit@gmail.com>
2024-08-19 21:41:33 +0530

    build fix

--- app/models/issuance.ts
+++ app/models/issuance.ts
@@ -1,5 +1,3 @@
-
-import { Button } from '../../button';
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
--- app/ui/explore/table-row.tsx
+++ app/ui/explore/table-row.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { BidAskCell } from '../market/bidAsk';
 import { Card, CardBody, Tooltip } from '@nextui-org/react';
-import { useRouter } from 'next/navigation'
-import Link from 'next/link'
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function TableRow({
   key,
@@ -25,7 +25,6 @@ export default function TableRow({
   isinHover: boolean;
   clickable: boolean;
 }) {
-
   function tableData(cell: any, index: number) {
     let value = null;
     if (cell instanceof Date) {
@@ -42,23 +41,32 @@ export default function TableRow({
         </td>
       );
     } else if (cell instanceof Object) {
-      value = (cell.url === undefined) ? (
-        <td
-          key={index + 1}
-          className={`mx-0 truncate bg-auto  px-2 py-${padding} font-medium hover:text-clip`}
-        >
-          <BidAskCell
-            units={cell.units}
-            price={cell.price}
-            closePrice={cell.closePrice}
-          />
-        </td>
-      ) : (<td
-        key={index + 1}
-        className={`mx-0 truncate bg-auto  pl-5 py-${padding}  font-medium`}>
-        <Link href={cell.url} className='no-underline text-[#35353A] focus:cursor-pointer hover:underline'>
-          {cell.value}
-        </Link > </td>)
+      value =
+        cell.url === undefined ? (
+          <td
+            key={index + 1}
+            className={`mx-0 truncate bg-auto  px-2 py-${padding} font-medium hover:text-clip`}
+          >
+            <BidAskCell
+              units={cell.units}
+              price={cell.price}
+              closePrice={cell.closePrice}
+              classNames=""
+            />
+          </td>
+        ) : (
+          <td
+            key={index + 1}
+            className={`mx-0 truncate bg-auto  pl-5 py-${padding}  font-medium`}
+          >
+            <Link
+              href={cell.url}
+              className="text-[#35353A] no-underline hover:underline focus:cursor-pointer"
+            >
+              {cell.value}
+            </Link>{' '}
+          </td>
+        );
     } else {
       value = (
         <td
@@ -71,25 +79,30 @@ export default function TableRow({
     }
     return value;
   }
-  const router = useRouter()
+  const router = useRouter();
   return (
     <tr
       key={key}
-      className={`text-sm h-auto border-b border-solid border-gray-200 group ${clickable && ("cursor-pointer")} ${clickable && ("hover:bg-gray-100")} py-${padding}`}
+      className={`text-sm group h-auto border-b border-solid border-gray-200 ${
+        clickable && 'cursor-pointer'
+      } ${clickable && 'hover:bg-gray-100'} py-${padding}`}
       onClick={() => clickable && router.push('/explore/debentures/1')}
     >
       {showSno && (
         <td
           key={0}
-          className={`sticky left-0 z-10 mx-0 whitespace-nowrap  bg-white px-2 font-medium py-${padding} ${clickable && ("group-hover:bg-gray-100")}`}
+          className={`sticky left-0 z-10 mx-0 whitespace-nowrap  bg-white px-2 font-medium py-${padding} ${
+            clickable && 'group-hover:bg-gray-100'
+          }`}
         >
           {(currentPage - 1) * itemsPerPage + sno + 1}
         </td>
       )}
       <td
         key={1}
-        className={`sticky ${showSno ? 'left-[42px]' : 'left-0'
-          } ${clickable && ("group-hover:bg-gray-100")} z-10 mx-0  whitespace-nowrap bg-white px-2 font-medium py-${padding}`}
+        className={`sticky ${showSno ? 'left-[42px]' : 'left-0'} ${
+          clickable && 'group-hover:bg-gray-100'
+        } z-10 mx-0  whitespace-nowrap bg-white px-2 font-medium py-${padding}`}
       >
         {isinHover ? (
           <Tooltip
@@ -97,11 +110,11 @@ export default function TableRow({
             closeDelay={500}
             content={
               <Card className="rounded-lg border border-solid border-gray-200 bg-white p-2">
-                <CardBody className="text-xxs flex flex-col gap-1 font-semibold text-gray-500">
-                  <p className="text-2xs m-0 p-0">
+                <CardBody className="flex flex-col gap-1 text-xxs font-semibold text-gray-500">
+                  <p className="m-0 p-0 text-2xs">
                     BSE Scrip&ensp;:&ensp;938742
                   </p>
-                  <p className="text-2xs m-0 p-0">
+                  <p className="m-0 p-0 text-2xs">
                     NSE Scrip&ensp;:&ensp;871REC28-N9
                   </p>
                 </CardBody>
