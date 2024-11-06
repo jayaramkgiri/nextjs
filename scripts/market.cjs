5a50208586923d450d0cc72e67e764416203aed6
Jayaram <jayaram@agrya.in>
2024-11-06 16:34:42 +0530

    syntax error

--- scripts/market.cjs
+++ scripts/market.cjs
@@ -32,32 +32,32 @@ function marketData(iss, key1, key2) {
 }
 
 async function migrateMarketData(date = new Date()) {
-  // await Promise.all([migrateBseMarketData(), migrateNseMarketData()]);
+  await Promise.all([migrateBseMarketData(), migrateNseMarketData()]);
   date.setHours(0, 0, 0, 0);
   const trading_iss = await prisma.market.findMany({ where: { date: date } });
   for (const iss of trading_iss) {
     try {
-    const openClose = marketData(iss, 'Open', 'Close');
-    const buyOrderSellOrder = marketData(iss, 'BuyOrder', 'SellOrder');
-    const buyPriceSellPrice = marketData(iss, 'BuyPrice', 'SellPrice');
-    await prisma.market.update({
-      where: { id: iss.id },
-      data: {
-        open: openClose['open'],
-        close: openClose['close'],
-        total_buy_order: buyOrderSellOrder['buyOrder'],
-        total_sell_order: buyOrderSellOrder['sellOrder'],
-        buy_price: buyPriceSellPrice['buyPrice'],
-        sell_price: buyPriceSellPrice['sellPrice'],
-        buy_volume:
-          buyPriceSellPrice['buyPrice'] * buyOrderSellOrder['buyOrder'],
-        sell_volume:
-          buyPriceSellPrice['sellPrice'] * buyOrderSellOrder['sellOrder'],
-      },
-    });
-  } catch (error) {
-    console.log(`Error updating market data for ${iss.isin} -> ${error}`);
+      const openClose = marketData(iss, 'Open', 'Close');
+      const buyOrderSellOrder = marketData(iss, 'BuyOrder', 'SellOrder');
+      const buyPriceSellPrice = marketData(iss, 'BuyPrice', 'SellPrice');
+      await prisma.market.update({
+        where: { id: iss.id },
+        data: {
+          open: openClose['open'],
+          close: openClose['close'],
+          total_buy_order: buyOrderSellOrder['buyOrder'],
+          total_sell_order: buyOrderSellOrder['sellOrder'],
+          buy_price: buyPriceSellPrice['buyPrice'],
+          sell_price: buyPriceSellPrice['sellPrice'],
+          buy_volume:
+            buyPriceSellPrice['buyPrice'] * buyOrderSellOrder['buyOrder'],
+          sell_volume:
+            buyPriceSellPrice['sellPrice'] * buyOrderSellOrder['sellOrder'],
+        },
+      });
+    } catch (error) {
+      console.log(`Error updating market data for ${iss.isin} -> ${error}`);
+    }
   }
 }
-
 migrateMarketData();
