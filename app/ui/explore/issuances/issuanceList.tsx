a3e2ed08feb215d5e6a6dbff479a848b5fe90250
jayaramkgiri <jayaram.cit@gmail.com>
2024-12-17 00:39:04 +0530

    mobile syntax fix

--- app/ui/explore/issuances/issuanceList.tsx
+++ app/ui/explore/issuances/issuanceList.tsx
@@ -40,6 +40,7 @@ export default function IssuanceList({
                 <BidAskCell
                   classNames="w-3/5"
                   units={1007686}
+                  volume={1000000}
                   price={10999999.12}
                   closePrice={10999999}
                   showRupee={false}
@@ -50,6 +51,7 @@ export default function IssuanceList({
                 <BidAskCell
                   classNames="w-3/5"
                   units={1007686}
+                  volume={1000000}
                   price={10999999.12}
                   closePrice={10999999}
                   showRupee={false}
